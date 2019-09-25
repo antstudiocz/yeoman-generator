293c1a26498166228f98a1bed98d084321907edb
Ales Mraz <am@antstudio.cz>
2019-09-25 16:22:35 +0200

    Fix Generator overwrite already create file

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,6 +1,7 @@
 'use strict';
 const Generator = require('yeoman-generator');
 const shell = require('shelljs');
+const chalk = require('chalk')
 
 module.exports = class extends Generator {
 
@@ -11,6 +12,7 @@ module.exports = class extends Generator {
     this.argument('name', { type: String, required: true });
     this.options.name = this._capitalizeFirstLetter(this.options.name)
     this.destination = {};
+    this.error = [];
   }
 
   _capitalizeFirstLetter(string)
@@ -66,50 +68,87 @@ module.exports = class extends Generator {
 
   createComponent() {
     const config = this.destination.componentPath
-    this.fs.copyTpl(
-      this.templatePath(`component.php`),
-      this.destinationPath(`${config.path}/${this.options.name}/${this.options.name}.php`), {
-        component: this.options.name,
-        componentLower: this._lowerCaseFirstLetter(this.options.name),
-        config: config
-      }
-    )
-    this.fs.copyTpl(
-      this.templatePath(`component.latte`),
-      this.destinationPath(`${config.path}/${this.options.name}/${this._lowerCaseFirstLetter(this.options.name)}.latte`), {
-        component: this._lowerCaseFirstLetter(this.options.name)
-      }
-    )
-    this.fs.copyTpl(
-      this.templatePath(`componentFactory.php`),
-      this.destinationPath(`${config.path}/${this.options.name}/I${this.options.name}Factory.php`), {
-        component: this.options.name,
-        config: config
-      }
-    )
-    if (!this.options.nogit) {
-      this.fs.commit([], () => {
-        if (shell.exec(`git add ${config.path}/${this.options.name}/I${this.options.name}Factory.php ${config.path}/${this.options.name}/${this._lowerCaseFirstLetter(this.options.name)}.latte ${config.path}/${this.options.name}/${this.options.name}.php`).code !== 0) {
-          shell.echo('Error: Git add fail');
-          shell.exit(1);
+
+    const checkFileExist = this.fs.read(`${config.path}/${this.options.name}/${this.options.name}.php`, {defaults: ''})
+
+    if (checkFileExist.length === 0) {
+      this.fs.copyTpl(
+        this.templatePath(`component.php`),
+        this.destinationPath(`${config.path}/${this.options.name}/${this.options.name}.php`), {
+          component: this.options.name,
+          componentLower: this._lowerCaseFirstLetter(this.options.name),
+          config: config
         }
-      })
+      )
+      this.fs.copyTpl(
+        this.templatePath(`component.latte`),
+        this.destinationPath(`${config.path}/${this.options.name}/${this._lowerCaseFirstLetter(this.options.name)}.latte`), {
+          component: this._lowerCaseFirstLetter(this.options.name)
+        }
+      )
+      this.fs.copyTpl(
+        this.templatePath(`componentFactory.php`),
+        this.destinationPath(`${config.path}/${this.options.name}/I${this.options.name}Factory.php`), {
+          component: this.options.name,
+          config: config
+        }
+      )
+      if (!this.options.nogit) {
+        this.fs.commit([], () => {
+          if (shell.exec(`git add ${config.path}/${this.options.name}/I${this.options.name}Factory.php ${config.path}/${this.options.name}/${this._lowerCaseFirstLetter(this.options.name)}.latte ${config.path}/${this.options.name}/${this.options.name}.php`).code !== 0) {
+            shell.echo('Error: Git add fail');
+            shell.exit(1);
+          }
+        })
+      }
+    } else {
+      this.error.push('Component already exist!')
     }
   }
 
   createAssets() {
     const config = this.destination.componentPath
-    if (config.type === 'project') {
-      const name = this._lowerCaseFirstLetter(this.options.name)
+    const name = this._lowerCaseFirstLetter(this.options.name)
+
+    const checkFileExist = this.fs.read(`${config.stylePath}/_${name}.scss`, {defaults: ''})
+
+    if (checkFileExist.length === 0) {
+      if (config.type === 'project') {
+        this.fs.copyTpl(
+          this.templatePath(`_component.scss`),
+          this.destinationPath(`${config.stylePath}/_${name}.scss`), {
+            component: name
+          }
+        )
+        if (!this.options.nogit) {
+          this.fs.commit([], () => {
+            if (shell.exec(`git add ${config.stylePath}/_${name}.scss`).code !== 0) {
+              shell.echo('Error: Git add fail');
+              shell.exit(1);
+            }
+          })
+        }
+      }
+    }
+  }
+
+  createTests() {
+    const config = this.destination.componentPath
+
+    const checkFileExist = this.fs.read(`${config.testPath}/${this.options.name}/${this.options.name}Test.phpt`, {defaults: ''})
+
+    if (checkFileExist.length === 0) {
       this.fs.copyTpl(
-        this.templatePath(`_component.scss`),
-        this.destinationPath(`${config.stylePath}/_${name}.scss`), {
-          component: name
+        this.templatePath(`componentTest.phpt`),
+        this.destinationPath(`${config.testPath}/${this.options.name}/${this.options.name}Test.phpt`), {
+          component: this.options.name,
+          name: this._lowerCaseFirstLetter(this.options.name),
+          config: config
         }
       )
       if (!this.options.nogit) {
         this.fs.commit([], () => {
-          if (shell.exec(`git add ${config.stylePath}/_${name}.scss`).code !== 0) {
+          if (shell.exec(`git add ${config.testPath}/${this.options.name}/${this.options.name}Test.phpt`).code !== 0) {
             shell.echo('Error: Git add fail');
             shell.exit(1);
           }
@@ -118,29 +157,17 @@ module.exports = class extends Generator {
     }
   }
 
-  createTests() {
-    const config = this.destination.componentPath
-    this.fs.copyTpl(
-      this.templatePath(`componentTest.phpt`),
-      this.destinationPath(`${config.testPath}/${this.options.name}/${this.options.name}Test.phpt`), {
-        component: this.options.name,
-        name: this._lowerCaseFirstLetter(this.options.name),
-        config: config
-      }
-    )
-    if (!this.options.nogit) {
-      this.fs.commit([], () => {
-        if (shell.exec(`git add ${config.testPath}/${this.options.name}/${this.options.name}Test.phpt`).code !== 0) {
-          shell.echo('Error: Git add fail');
-          shell.exit(1);
-        }
+  writing() {
+    if (this.error.length === 0) {
+      this.log(`create new component with name ${this.options.name}`);
+      this.log(`${this.destination.componentPath.namespace}${this.options.name}\\I${this.options.name}Factory`)
+    } else {
+      this.error.forEach((error) => {
+        this.log(chalk.bold.white.bgRed(`--------------------------`));
+        this.log(chalk.bold.white.bgRed(` ${error} `));
+        this.log(chalk.bold.white.bgRed(`--------------------------`));
       })
     }
-  }
-
-  writing() {
-    this.log(`create new component with name ${this.options.name}`);
-    this.log(`${this.destination.componentPath.namespace}${this.options.name}\\I${this.options.name}Factory`)
 
     if (!this.options.nogit) {
       if (!shell.which('git')) {
